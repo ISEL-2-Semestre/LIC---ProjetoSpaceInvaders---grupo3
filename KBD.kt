d309e31b4e4a65b420457563910a4b47bd7f2ae1
FreddieSilver <62643295+FreddieSilver@users.noreply.github.com>
2024-03-25 09:19:53 +0000

    KBD.kt

--- KBD.kt
+++ KBD.kt
@@ -1,28 +1,36 @@
-import isel.leic.UsbPort
 import isel.leic.utils.Time
 
 const val DVAL_MASK = 0x10
-const val smask = 0xF
-val outputd = HAL.readBits(DVAL_MASK)
-val output = HAL
+const val SMASK = 0x0F
+val output = HAL.readBits(SMASK)
+val teclado = charArrayOf('1', '4', '7', '*',
+    '2', '5', '8', '0',
+    '3', '6', '9', '#')
 
 object KBD { // Ler teclas. Métodos retornam ‘0’..’9’,’#’,’*’ ou NONE.
-
-    const val NONE = '0';
+    var keypress = false
+    const val NONE = 0;
     var output = 0
-    // Inicia a classe
-    fun init(){
-        UsbPort.write(output)
+    fun init() {
+        keypress = HAL.isBit(DVAL_MASK)
     }
-// Retorna de imediato a tecla premida ou NONE se não há tecla premida.
-    fun getKey(): Char {
 
+    // Retorna de imediato a tecla premida ou NONE se não há tecla premida.
+    fun getKey(): Char {
+        keypress = HAL.isBit(DVAL_MASK)
+        if (keypress) return teclado[HAL.readBits(SMASK)]
+        else return NONE.toChar()
     }
-// Retorna a tecla premida, caso ocorra antes do ‘timeout’ (representado em milissegundos), ou
-    fun waitKey(timeout: Long): Char{
-        return if (timeout - Time.getTimeInMillis() >=0 && HAL.isBit(DVAL_MASK)){
+
+    fun waitKey(timeout: Long): Char {
+        return if (timeout - Time.getTimeInMillis() >= 0 && HAL.isBit(DVAL_MASK)) {
             getKey()
-        }
-        else NONE
+        } else NONE.toChar()
     }
+}
+
+fun main() {
+    HAL.init()
+    KBD.init()
+    println(KBD.getKey())
 }
