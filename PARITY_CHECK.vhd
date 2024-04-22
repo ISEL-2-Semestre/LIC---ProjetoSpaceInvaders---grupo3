604f55d59ac2500db0b0136664e283e24ef5d16a
TiagoPraia <161584193+TiagoPraia@users.noreply.github.com>
2024-04-22 12:04:35 +0100

    Update PARITY_CHECK.vhd

--- PARITY_CHECK.vhd
+++ PARITY_CHECK.vhd
@@ -2,31 +2,32 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity PARITY_CHECK is 
-    port(
-    data, clk, Reset,  init: in std_logic;
-    err: out std_logic
-    );
+port(	data 		: in std_logic;
+		clk 		: in std_logic;
+		init		: in std_logic;
+		RESET 	    : in std_logic;
+		err		    : out std_logic
+		);
 end PARITY_CHECK;
 
-architecture PARITY_CHECK_Arch of PARITYCHECK is
+architecture structure of PARITY_CHECK is
 
-component CounterUp is
-    port(
-    dataIn : in std_logic_vector(3 downto 0);
-    PL : in std_logic;
-    CE: in std_logic;
-    CLK : in std_logic;
-    RESET : in std_logic;
-    Q : out std_logic_vector(3 downto 0)
-    );
+component COUNTER_UP is
+port(	dataIn	    : in std_logic_vector(3 downto 0);
+		PL 		    : in std_logic;
+		CE			: in std_logic;
+		CLK 		: in std_logic;
+		RESET	 	: in std_logic;
+		Q 			: out std_logic_vector(3 downto 0)
+		);
 end component;
 
 signal sum: std_logic_vector(3 downto 0);
 
 begin
 
-Cont: CounterUp port map (dataIn => "0000" , PL => '0', CE => data, CLK => Clk , RESET => Reset, Q => sum);
+Cont: COUNTER_UP port map (dataIn => "0000" , PL => '0', CE => data, CLK => Clk , RESET => RESET, Q => sum);
 
 err <= sum(0);
 
-end PARITY_CHECK_Arch;
+end structure;
