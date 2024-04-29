a82b7e54d2635b263ad9a33bafee07262533d560
TiagoPraia <161584193+TiagoPraia@users.noreply.github.com>
2024-04-29 12:15:19 +0100

    Update SCORE_DISPATCHER.vhd

--- SCORE_DISPATCHER.vhd
+++ SCORE_DISPATCHER.vhd
@@ -3,11 +3,11 @@ USE ieee.std_logic_1164.all;
 
 Entity SCORE_DISPATCHER is
 Port( Dval : in std_logic;
-		Din : in std_logic_vector(8 downto 0);
+		Din : in std_logic_vector(6 downto 0);
 		RESET : in std_logic;
 		CLK : in std_logic;
 		WrD : out std_logic;
-		Dout : out std_logic_vector(8 downto 0);
+		Dout : out std_logic_vector(6 downto 0);
 		done : out std_logic
 		);
 End SCORE_DISPATCHER;
