7991865483f8b92b9283aeb35c0da77277bf155c
FreddieSilver <62643295+FreddieSilver@users.noreply.github.com>
2024-05-21 17:56:07 +0100

    Add files via upload

--- SerialEmitter.kt
+++ SerialEmitter.kt
@@ -10,47 +10,41 @@ object SerialEmitter {
     fun init(){
         HAL.init()
         HAL.setBits(LCDset_MASK)
+        HAL.setBits(SCLK_MASK)
 
     }
     // Envia uma trama para o SerialReceiver identificado o destino em addr,os bits de dados em
     // ‘data’ e em size o número de bits a enviar.
-    fun send(addr: Destination, data: Int, size : Int){
-        when(addr) {
-            Destination.LCD -> {
-                HAL.clrBits(LCDset_MASK)
-                for (i in 0..size) {
-                    HAL.clrBits(SCLK_MASK)
-                    val bit = data.and(1.shl(i))
-                    if (bit == 0) {
-                        HAL.clrBits(SDX_MASK)
-                    } else {
-                        HAL.setBits(SDX_MASK)
-                    }
-                    HAL.setBits(SCLK_MASK)
-                }
-                HAL.clrBits(SCLK_MASK)
-                HAL.clrBits(LCDset_MASK)
+    fun send(addr: Destination, data: Int, size: Int) {
 
+        var NOTSS_MASK = 0
+        if(addr == Destination.LCD){NOTSS_MASK = LCDset_MASK}
+        else{NOTSS_MASK = SCLK_MASK}
+
+        HAL.clrBits(NOTSS_MASK)
+        var counter = 0
+        for (i in 0..size-1) {
+
+            val bit = data.and(1.shl(i))
+            if (bit == 0) {
+                HAL.clrBits(SDX_MASK)
+            } else {
+                HAL.setBits(SDX_MASK)
+                counter++
             }
-
-            Destination.SCORE -> {
-                HAL.clrBits(LCDset_MASK)
-                for (i in 0..size){
-                    HAL.clrBits(SCLK_MASK)
-                    val bit = data.and(1.shl(i))
-                    if(bit == 0){
-                        HAL.clrBits(SDX_MASK)
-                    }
-                    else{HAL.setBits(SDX_MASK)}
-                    HAL.setBits(SCLK_MASK)
-                }
-                HAL.clrBits(SCLK_MASK)
-
-            }
-
-            }
+            HAL.setBits(SCLK_MASK)
+            HAL.clrBits(SCLK_MASK)
         }
+        if (counter % 2 == 0) {
+            HAL.clrBits(SDX_MASK)
+        } else {
+            HAL.setBits(SDX_MASK)
+        }
+        HAL.setBits(SCLK_MASK)
+        HAL.clrBits(SCLK_MASK)
+        HAL.setBits(NOTSS_MASK)
     }
+}
 
 fun main() {
     SerialEmitter.init()
