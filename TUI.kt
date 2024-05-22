c5730463f9bb85a928cb626f8316a5fb87b47061
TiagoPraia <161584193+TiagoPraia@users.noreply.github.com>
2024-05-22 14:57:30 +0100

    Update TUI.kt

--- TUI.kt
+++ TUI.kt
@@ -1,32 +1,35 @@
-import isel.leic.utils.Time
+object TUI{
+    fun init() {
+        HAL.init()
+        KBD.init()
+        LCD.init()
+        SerialEmitter.init()
+        var line = 0
 
-fun main() {
-    HAL.init()
-    KBD.init()
-    LCD.init()
-    var line = 0
-
-    while (true) {
-        val key = KBD.waitKey(1000) // espera 1 segundo ou obtém a tecla pressionada
-        when (key) {
-            '0' -> { LCD.write(key) }
-            '1' -> {  LCD.write(key)}
-            '2' -> { LCD.write(key) }
-            '3' -> {  LCD.write(key)}
-            '4' -> { LCD.write(key) }
-            '5' -> { LCD.write(key) }
-            '6' -> {LCD.write(key)  }
-            '7' -> {LCD.write(key)  }
-            '8' -> { LCD.write(key) }
-            '9' -> {LCD.write(key)  }
-            // adicione mais casos aqui para outras teclas
-            '*' -> {
-                LCD.clear() // apaga o display
-            }
-            '#' -> {
-                line = 1 - line // muda de linha
-                LCD.cursor(line, 0)
+        while (true) {
+            val key = KBD.waitKey(1000) // espera 1 segundo ou obtém a tecla pressionada
+            when (key) {
+                '0' -> { LCD.write(key) }
+                '1' -> {  LCD.write(key)}
+                '2' -> { LCD.write(key) }
+                '3' -> {  LCD.write(key)}
+                '4' -> { LCD.write(key) }
+                '5' -> { LCD.write(key) }
+                '6' -> {LCD.write(key)  }
+                '7' -> {LCD.write(key)  }
+                '8' -> { LCD.write(key) }
+                '9' -> {LCD.write(key)  }
+                // adicione mais casos aqui para outras teclas
+                '*' -> {
+                    LCD.clear() // apaga o display
+                }
+                '#' -> {
+                    line = 1 - line // muda de linha
+                    LCD.cursor(line, 0)
+                }
             }
         }
     }
 }
+
+
