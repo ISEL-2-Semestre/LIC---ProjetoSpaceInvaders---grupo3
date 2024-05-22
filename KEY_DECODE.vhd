f27aaa7fc503ff71d7cb9db4935862c5c31c1c49
Franciscoevora <112759682+Franciscoevor@users.noreply.github.com>
2024-05-22 21:54:35 +0100

    Update KEY_DECODE.vhd

--- KEY_DECODE.vhd
+++ KEY_DECODE.vhd
@@ -21,11 +21,11 @@ Port( Kscan : in std_logic;
 		);
 end component;
 
-Component CLK_DIV is
-generic(div: natural := 50000000);
-port( clk_in: in std_logic;
-		clk_out: out std_logic);
-end component;
+--Component CLK_DIV is
+--generic(div: natural := 16);
+--port( clk_in: in std_logic;
+--		clk_out: out std_logic);
+--end component;
 
 Component KEY_CONTROL is
 Port( Kack, Kpress, CLK, RESET : in std_logic;
@@ -40,13 +40,13 @@ signal carry_Kpress : std_logic;
 
 Begin
 
-U1 : KEY_CONTROL port map(	Kack => Kack, Kpress => carry_Kpress, CLK => carry_CLK, RESET => RESET, 
+U1 : KEY_CONTROL port map(	Kack => Kack, Kpress => carry_Kpress, CLK => CLK, RESET => RESET, 
 									Kscan => carry_Kscan, Kval => Kval);
 
-U2 : KEY_SCAN 	port map(	Kscan => carry_Kscan, CLK => carry_CLK, Reset => RESET, lin0 => LIN(0), lin1 => LIN(1),
+U2 : KEY_SCAN 	port map(	Kscan => carry_Kscan, CLK => CLK, Reset => RESET, lin0 => LIN(0), lin1 => LIN(1),
 									lin2 => LIN(2), lin3 => LIN(3), Dout => COL, Kpress => carry_Kpress, K => K);
 								
-U3 : CLK_DIV 	port map( 	clk_in => CLK , clk_out => carry_CLK);
+--U3 : CLK_DIV 	port map( 	clk_in => CLK , clk_out => carry_CLK);
 									
 									
 end structural;
